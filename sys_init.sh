68d340ea47ef162d8343fff114347ff403afbedd
Christian Schmitt <chrischmo@gmail.com>
2014-11-13 13:43:51 +0100

    Initial commit of my system and user setup scripts.

---
+++ sys_init.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo "========================================================================"
+echo "Executing System Setup Script:"
+echo "========================================================================"
+
+
+echo "Updating/Upgrading apt: ================================================"
+apt-get update && apt-get upgrade -y
+
+
+echo "Installing essential tools: ============================================"
+apt-get install build-essential cmake libxml2 libxml2-dev libxslt1-dev -y
+apt-get install zsh tree dtrx ncurses-term aptitude curl sudo htop ppa-purge -y
+apt-get install vim-nox git tmux -y
+
+
+echo "Installing python-specific tools: ======================================"
+apt-get install python-dev pylint python-pip -y
+
+
+echo "Setting zsh as default shell: =========================================="
+chsh -s /bin/zsh
+
+echo "========================================================================"
+echo "Finished System Setup Script. Exit & reconnect to continue with 2nd part"
+echo "========================================================================"
---
+++ user_init.sh
@@ -0,0 +1,27 @@
+#!/bin/zsh
+
+#echo "Installing Prezto: ====================================================="
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+echo "Installing Powerline: =================================================="
+apt-get install socat -y
+pip install psutil pyuv i3-py
+pip install git+git://github.com/Lokaltog/powerline
+
+# TODO (gehört in /etc/zsh/zshrc - ggf. nicht ans Ende??):
+#if [[ -r /usr/local/lib/python2.7/dist-packages/powerline/bindings/zsh/powerline.zsh ]]; then
+#    source /usr/local/lib/python2.7/dist-packages/powerline/bindings/zsh/powerline.zsh
+#fi
+
+echo "TODO:"
+# echo "Set zsh as default shell with 'csh -s /bin/zsh' (requires password)"
+echo "Install powerline"
+echo "Append 'source /usr/local/lib/python2.7/dist-packages/powerline/bindings/zsh/powerline.zsh' to '/etc/zsh/zshrc'"
+echo "Install neobundle and bundles (syntastic, youcompleteme, vim-fugitive, gundo.vim, vim-airline"
+echo "Change root password with 'passwd'"
+echo "Create regular user"
+echo "========================================================================"
