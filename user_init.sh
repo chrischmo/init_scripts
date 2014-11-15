d465b1452bd6169f610b66f6cd3cd6f040a1f3ac
Christian Schmitt <chrischmo@gmail.com>
2014-11-15 06:50:53 +0000

    Commentary and line spacing cleanup.

--- user_init.sh
+++ user_init.sh
@@ -1,7 +1,17 @@
 #!/bin/zsh
 
+echo "========================================================================"
+echo "User setup starting..."
+echo "Please make sure you run this script from your home directory."
+echo "The following requirements are implied:"
+echo "- This command is run from within zsh (for the other requirements, see 'sys_init.sh'"
+echo "- The user has installed his/her public key for github (use ssh-add)."
+echo "========================================================================"
+
+
 echo "Installing NeoBundle:  ====================================================="
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | zsh
+
 
 echo "Installing Prezto: ====================================================="
 git clone --recursive https://github.com/chrischmo/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
@@ -10,16 +20,25 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
+
 echo "Installing Powerline: =================================================="
 pip install psutil pyuv i3-py
 pip install --user git+git://github.com/Lokaltog/powerline
 
-# Add ~/.local/bin to path
+# Add ~/.local/bin to path:
 cat >>$HOME/.zprofile <<EOL
 if [ -d "$HOME/.local/bin" ]; then
     PATH="$HOME/.local/bin:$PATH"
 fi
 EOL
+
+# Activate zsh plugin:
+cat >>$HOME/.zshrc <<EOL
+if [[ -r ~/.local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh ]]; then
+    source ~/.local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
+fi
+EOL
+
 
 echo "Setting up vcsh: ==========================================="
 # Clone the myrepos template:
@@ -32,7 +51,6 @@ vcsh mr remote add origin git@github.com:chrischmo/vcsh_mr_template.git
 vcsh mr push --set-upstream origin master
 
 
-echo "TODO:"
-echo "Change root password with 'passwd'"
-echo "Create regular user"
+echo "========================================================================"
+echo "User setup finished - please check sys_out for possible errors!"
 echo "========================================================================"
